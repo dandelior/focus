b96b0524429f58faa1efce67572a7a2db0126819
Juan Berrios <jberrioscifuentes@gmail.com>
2021-03-16 18:19:21 -0300

    posts que estan listos

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { getAllPosts } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 export default function Home({ allPosts }) {
 
   const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  const lastPosts = allPosts;
 
   return (
     <div>
@@ -21,7 +21,7 @@ export default function Home({ allPosts }) {
         index.js
       </p>
 
-      {morePosts.map((post) => (
+      {lastPosts.map((post) => (
         <>
           <h1>{post.title}</h1>
           <p>{post.date}</p>
